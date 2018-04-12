3c737789b954f012d509eb835458662762a75b25
Junior Silva <juniorpsilva7@gmail.com>
2018-04-11 23:34:17 -0300

    Final - Employee API done with SQL context, Repository, Migrations and Seed an empty Database

---
+++ LLEmployees/Migrations/20180411183209_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LLEmployees.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Department = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ LLEmployees/Models/DBInitializer.cs
@@ -0,0 +1,33 @@
+﻿using LLEmployees.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace LLEmployees.Models
+{
+    public class DBInitializer
+    {
+        private static EmployeeContext _context;
+
+        public static void Seed(EmployeeContext context)
+        {
+            //EmployeeContext _context =
+            //    applicationBuilder.ApplicationServices.GetRequiredService<EmployeeContext>();
+
+            _context = context;
+
+            if (_context.Employees.Count() == 0)
+            {
+                _context.Add( new Employee { Name = "Rodrigo Carvalho", Email = "rodrigo@luizalabs.com", Department = "IntegraCommerce" });
+                _context.Add( new Employee { Name = "Renato Pedigoni", Email = "renato@luizalabs.com", Department = "Digital Platform" });
+                _context.Add( new Employee { Name = "Thiago Catoto", Email = "catoto@luizalabs.com", Department = "Mobile" });
+                
+                _context.SaveChanges();
+            }
+        }
+    }
+}
